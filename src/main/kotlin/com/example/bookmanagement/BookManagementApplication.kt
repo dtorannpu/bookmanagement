8b5a03a30c5a1866b139386dc202bd48dafa2761
dtorannpu <dtorannpu@gmail.com>
2024-04-03 10:48:25 +0900

    ktlintFormat適用

--- src/main/kotlin/com/example/bookmanagement/BookManagementApplication.kt
+++ src/main/kotlin/com/example/bookmanagement/BookManagementApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class BookManagementApplication
+class BookmanagementApplication
 
 fun main(args: Array<String>) {
-	runApplication<BookManagementApplication>(*args)
+    runApplication<BookmanagementApplication>(*args)
 }
