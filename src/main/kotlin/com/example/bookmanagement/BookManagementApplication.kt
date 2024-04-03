f12dc47705da92ffe5eb7c1cd01d02ad16b2b7ef
dtorannpu <dtorannpu@gmail.com>
2024-04-03 10:35:09 +0900

    クラス名修正

--- src/main/kotlin/com/example/bookmanagement/BookManagementApplication.kt
+++ src/main/kotlin/com/example/bookmanagement/BookManagementApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class BookmanagementApplication
+class BookManagementApplication
 
 fun main(args: Array<String>) {
-	runApplication<BookmanagementApplication>(*args)
+	runApplication<BookManagementApplication>(*args)
 }
