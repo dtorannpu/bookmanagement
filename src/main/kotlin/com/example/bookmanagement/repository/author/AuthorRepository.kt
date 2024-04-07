31311513389abdbe5f7fac0d5a3d5bc96b988aea
dtorannpu <dtorannpu@gmail.com>
2024-04-07 13:37:56 +0900

    AuthorDetailからAuthorに変更

--- src/main/kotlin/com/example/bookmanagement/repository/author/AuthorRepository.kt
+++ src/main/kotlin/com/example/bookmanagement/repository/author/AuthorRepository.kt
@@ -1,6 +1,6 @@
 package com.example.bookmanagement.repository.author
 
-import com.example.bookmanagement.model.AuthorDetail
+import com.example.bookmanagement.model.Author
 import java.time.LocalDate
 
 /**
@@ -36,5 +36,5 @@ interface AuthorRepository {
      * @param id ID
      * @return 著者
      */
-    fun findById(id: Int): AuthorDetail?
+    fun findById(id: Int): Author?
 }
--- src/main/kotlin/com/example/bookmanagement/service/author/AuthorServiceImpl.kt
+++ src/main/kotlin/com/example/bookmanagement/service/author/AuthorServiceImpl.kt
@@ -1,6 +1,6 @@
 package com.example.bookmanagement.service.author
 
-import com.example.bookmanagement.model.AuthorDetail
+import com.example.bookmanagement.model.Author
 import com.example.bookmanagement.repository.author.AuthorRepository
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -29,7 +29,7 @@ class AuthorServiceImpl(private val authorRepository: AuthorRepository) :
         authorRepository.update(id, name, birthday)
     }
 
-    override fun findById(id: Int): AuthorDetail? {
+    override fun findById(id: Int): Author? {
         return authorRepository.findById(id)
     }
 }
