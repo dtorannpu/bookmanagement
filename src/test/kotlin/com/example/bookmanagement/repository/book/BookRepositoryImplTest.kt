b4260bda2ba3d64f90e9a25e498cdf472686182d
dtorannpu <dtorannpu@gmail.com>
2024-04-10 08:19:52 +0900

    書籍の更新テスト追加

--- src/test/kotlin/com/example/bookmanagement/repository/book/BookRepositoryImplTest.kt
+++ src/test/kotlin/com/example/bookmanagement/repository/book/BookRepositoryImplTest.kt
@@ -15,9 +15,11 @@ import org.testcontainers.junit.jupiter.Testcontainers
 import java.time.LocalDate
 import kotlin.test.assertEquals
 import kotlin.test.assertNotNull
+import kotlin.test.assertNull
 
 @SpringBootTest
 @Testcontainers
+@Transactional
 class BookRepositoryImplTest
     @Autowired
     constructor(
@@ -96,5 +98,46 @@ class BookRepositoryImplTest
             assertEquals(changeAfterAuthorAuthorId, actual.authorId)
             assertEquals("0123456789", actual.isbn)
             assertEquals("坊ちゃん", actual.title)
+        }
+
+        @Test
+        fun testBookSearchNoCondition() {
+            val author1 = create.newRecord(AUTHOR)
+            author1.name = "山田　太郎"
+            author1.birthday = LocalDate.of(2023, 5, 13)
+            author1.store()
+            val author1Id = author1.id!!
+
+            val book1 = create.newRecord(BOOK)
+            book1.isbn = "1234567890"
+            book1.title = "こころ"
+            book1.authorId = author1Id
+            book1.store()
+            val book1Id = book1.id!!
+
+            val author2 = create.newRecord(AUTHOR)
+            author2.name = "山田　次郎"
+            author2.store()
+            val author2Id = author2.id!!
+
+            val book2 = create.newRecord(BOOK)
+            book2.title = "坊ちゃん"
+            book2.authorId = author2Id
+            book2.store()
+            val book2Id = book2.id!!
+
+            val actual = bookRepository.search(null, null, null)
+
+            assertEquals(2, actual.size)
+            val resultBook1 = actual[0]
+            val resultBook2 = actual[1]
+
+            assertEquals(book1Id, resultBook1.id)
+            assertEquals("1234567890", resultBook1.isbn)
+            assertEquals("こころ", resultBook1.title)
+
+            assertEquals(book2Id, resultBook2.id)
+            assertNull(resultBook2.isbn)
+            assertEquals("坊ちゃん", resultBook2.title)
         }
     }
