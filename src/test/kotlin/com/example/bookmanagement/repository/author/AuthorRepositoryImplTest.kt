96deac9e685903863cfd2ffbe130a626486e0cb5
dtorannpu <dtorannpu@gmail.com>
2024-04-10 12:29:27 +0900

    トランザクションアノテーション追加

--- src/test/kotlin/com/example/bookmanagement/repository/author/AuthorRepositoryImplTest.kt
+++ src/test/kotlin/com/example/bookmanagement/repository/author/AuthorRepositoryImplTest.kt
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.DynamicPropertyRegistry
 import org.springframework.test.context.DynamicPropertySource
+import org.springframework.transaction.annotation.Transactional
 import org.testcontainers.containers.PostgreSQLContainer
 import org.testcontainers.junit.jupiter.Testcontainers
 import java.time.LocalDate
@@ -20,6 +21,7 @@ import kotlin.test.assertTrue
 
 @SpringBootTest
 @Testcontainers
+@Transactional
 class AuthorRepositoryImplTest
     @Autowired
     constructor(
