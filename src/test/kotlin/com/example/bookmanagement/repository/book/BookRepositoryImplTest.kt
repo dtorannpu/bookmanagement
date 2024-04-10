757a451763641efa591ae5a6de6f1a93a5655846
dtorannpu <dtorannpu@gmail.com>
2024-04-10 12:38:49 +0900

    チェックエラー修正

--- src/test/kotlin/com/example/bookmanagement/repository/book/BookRepositoryImplTest.kt
+++ src/test/kotlin/com/example/bookmanagement/repository/book/BookRepositoryImplTest.kt
@@ -10,13 +10,13 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.DynamicPropertyRegistry
 import org.springframework.test.context.DynamicPropertySource
+import org.springframework.transaction.annotation.Transactional
 import org.testcontainers.containers.PostgreSQLContainer
 import org.testcontainers.junit.jupiter.Testcontainers
 import java.time.LocalDate
 import kotlin.test.assertEquals
 import kotlin.test.assertNotNull
 import kotlin.test.assertNull
-import org.springframework.transaction.annotation.Transactional
 
 @SpringBootTest
 @Testcontainers
